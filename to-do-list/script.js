6b001872e3cde8b68fc8baa152ff6e282fbb539e
Bongumusa Gumede <bonggums@gmail.com>
2025-10-05 23:12:09 +0200

    TO-DO-LIST

---
+++ to-do-list/script.js
@@ -0,0 +1,50 @@
+const InputBox = document.getElementById("input-box");
+const listContaner = document.getElementById("list-container");
+
+
+function Addtask(){
+    if(InputBox.value === ''){
+        alert("you must write someting");
+    }
+    else{
+        let li =document.createElement('li');
+        li.innerHTML = InputBox.value;
+        listContaner.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+
+    InputBox.value ='';
+    saveData();
+
+}
+
+
+listContaner.addEventListener('click', function(e){
+    if(e.target.tagName ==='LI'){
+        e.target.classList.toggle("checked");
+        saveData();
+
+    }
+    else if(e.target.tagName === 'SPAN'){
+
+    e.target.parentElement.remove();
+    saveData();
+
+    }
+}, false);
+
+
+
+function saveData(){
+    localStorage.setItem("data", listContaner.innerHTML);
+}
+
+function showTask(){
+    listContaner.innerHTML = localStorage.getItem('data');
+
+}
+
+showTask();
